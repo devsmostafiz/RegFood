92112801a46b030510c4ee703332e6ddf5252f0b
devsmostafiz <mostafijurrhaman87@gmail.com>
2023-11-25 18:36:10 +0600

    secound update

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$('.offer_item_wrapper').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:false,
+    autoplay:true,
+    responsive: [
+      {
+        breakpoint: 1209,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
