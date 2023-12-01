6723b6d4fd699b902bae352f7c752f2040a1c3cc
devsmostafiz <mostafijurrhaman87@gmail.com>
2023-12-01 20:29:32 +0600

    theree more update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $('.offer_item_wrapper').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows:true,
-    prevArrow: '.clck_arrow_prv',
-    nextArrow: '.clck_arrow_next',
+    prevArrow: '.offer_arrow_prv',
+    nextArrow: '.offer_arrow_next',
     autoplay:true,
     responsive: [
       {
@@ -62,7 +62,7 @@ $('.offer_item_wrapper').slick({
     arrows:true,
     prevArrow: '.clck_arrow_prv',
     nextArrow: '.clck_arrow_next',
-    autoplay:false,
+    autoplay:true,
     responsive: [
       {
         breakpoint: 1209,
@@ -94,8 +94,92 @@ $('.offer_item_wrapper').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+    ]
+  });
+
+  // ========= Easy Order Area Here ===========
+
+  $('.easy_item_wrapper').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1209,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  // ========= Testimonial Area Here ===========
+
+  $('.testimonial_wrapper').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows:true,
+    prevArrow: '.testimonial_arrow_prv',
+    nextArrow: '.testimonial_arrow_next',
+    autoplay:true,
+    responsive: [
+      {
+        breakpoint: 1209,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
     ]
   });
